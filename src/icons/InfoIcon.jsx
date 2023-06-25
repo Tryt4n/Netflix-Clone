0a6bcfa274500ce820b1fe6eb181a99182cbd009
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2023-06-25 13:16:22 +0200

    Done PINCodePage, added translations

---
+++ src/icons/InfoIcon.jsx
@@ -0,0 +1,18 @@
+export default function InfoIcon() {
+  return (
+    <svg
+      className="svg-icon svg-icon-info"
+      fill="none"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      data-name="Info"
+      aria-label="Info Icon"
+    >
+      <path
+        d="M12 3a9 9 0 100 18 9 9 0 000-18zM1 12C1 5.925 5.925 1 12 1s11 4.925 11 11-4.925 11-11 11S1 18.075 1 12zm12-2v8h-2v-8h2zm-1-1.5a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}
--- src/pages/PINCodePage/PINCodePage.jsx
+++ src/pages/PINCodePage/PINCodePage.jsx
@@ -9,9 +9,10 @@ import AccountFooter from "../../layout/AccountFooter/AccountFooter";
 import AccountSettingsBtn from "../../components/AccountSettingsBtn/AccountSettingsBtn";
 
 import "./pinCodePage.scss";
+import InfoIcon from "../../icons/InfoIcon";
 
 export default function PINCodePage() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const {
     users,
@@ -30,6 +31,10 @@ export default function PINCodePage() {
   const [codePIN, setCodePIN] = useState(isLockedUser ? currentEditingProfile.PIN : "");
   const [isInputFocused, setIsInputFocused] = useState(false);
   const [selectedCharIndex, setSelectedCharIndex] = useState(0);
+  const [isErrorMessageVisible, setIsErrorMessageVisible] = useState(false);
+  const [requirePinForNewAccount, setRequirePinForNewAccount] = useState(
+    users[0].requirePinToCreateAccount
+  );
   ////////////////////////////////////////////////////////////////////////////////////////*
 
   const pinCodeRef = useRef(null);
@@ -66,13 +71,28 @@ export default function PINCodePage() {
         newCodePIN[selectedCharIndex] = e.key;
         setCodePIN(newCodePIN.join(""));
         setSelectedCharIndex((selectedCharIndex + 1) % 4);
+
+        if (newCodePIN.length === 4 && selectedCharIndex === 3) {
+          pinCodeRef.current.blur();
+          const nextElement = pinCodeRef.current.nextElementSibling;
+          if (nextElement) {
+            nextElement.focus();
+          }
+        }
       }
     }
   }
 
   function handleSave() {
     const updatedUsers = users.map((user) => {
-      if (user.username === currentEditingProfile.username) {
+      if (users[0].username === currentEditingProfile.username) {
+        return {
+          ...user,
+          lock: isLockedUser,
+          PIN: isLockedUser ? codePIN : "",
+          requirePinToCreateAccount: requirePinForNewAccount,
+        };
+      } else if (user.username === currentEditingProfile.username) {
         return {
           ...user,
           lock: isLockedUser,
@@ -83,9 +103,10 @@ export default function PINCodePage() {
     });
     setUsers(updatedUsers);
     setIsCurrentlySaved(true);
-    setDisplayedSavedMessage("Ustawiono PIN");
+    setDisplayedSavedMessage(t("profileLockSaveMessage"));
     resetPasswordConfirmationSettings();
     setIsBtnDisabled(true);
+    setIsErrorMessageVisible(false);
   }
 
   return (
@@ -101,136 +122,150 @@ export default function PINCodePage() {
       <div className="restriction-confirmation">
         <main className="restriction-confirmation__content-container">
           <header className="restriction-confirmation__header">
-            <h1 className="restriction-confirmation__heading">{t("viewingRestrictions")}</h1>
+            <h1 className="restriction-confirmation__heading">{t("profileLock")}</h1>
             <img
               className="restriction-confirmation__profile-img"
               src={currentEditingProfile?.profileImage}
               alt={`${t("profileAvatar")} ${currentEditingProfile?.username}`}
             />
           </header>
-          <PasswordConfirmation
-            textDescription={"Wprowadź hasło do konta, aby edytować blokadę profilu użytkownika"}
-          />
+          <PasswordConfirmation textDescription={t("profileLockDescription")} />
 
           {passwordConfirmationPassed && (
             <>
               <section>
-                <h2 className="restriction-confirmation__subheading">
-                  Ustaw blokadę profilu, wybierając 4-cyfrowy kod PIN.
-                </h2>
+                <h2 className="restriction-confirmation__subheading">{t("lockThisProfile")}</h2>
                 <form
                   onClick={() => {
                     setIsBtnDisabled(false);
                   }}
                 >
-                  <input
-                    type="checkbox"
-                    name="pin-checkbox"
-                    id="pin-checkbox"
-                    className="checkbox-light"
-                    checked={isLockedUser}
-                    onChange={handleInputChange}
-                  />
-                  <label
-                    htmlFor="pin-checkbox"
-                    className="checkbox-light-label"
-                  >
-                    Żądaj podania kodu PIN do profilu Marcin.
-                  </label>
+                  <div className="pin-page__require-pin-code-wrapper">
+                    <input
+                      type="checkbox"
+                      name="pin-checkbox"
+                      id="pin-checkbox"
+                      className="checkbox-light"
+                      checked={isLockedUser}
+                      onChange={handleInputChange}
+                    />
+                    <label
+                      htmlFor="pin-checkbox"
+                      className="checkbox-light-label pin-page__label"
+                    >
+                      {t("requirePIN")} {currentEditingProfile?.username}
+                      {i18n.language === "en" ? `'s ${t("profile")}` : ""}.
+                      <span
+                        className="pin-page__require-pin-code-tooltip"
+                        aria-label={t("requirePINTooltip")}
+                      >
+                        <InfoIcon />
+                      </span>
+                    </label>
+                  </div>
 
                   {isLockedUser && (
                     <>
-                      <fieldset aria-errormessage="pin-warning-message">
-                        <legend className="visually-hidden">Wprowadź kod PIN</legend>
-                        <div className="visually-hidden">
-                          <label htmlFor="pin-code">Wprowadź kod PIN</label>
-                          <input
-                            type="tel"
-                            name="pin-code"
-                            id="pin-code"
-                            maxLength={4}
-                            pattern="[0-9]*"
-                            autoComplete="off"
-                            value={codePIN}
-                            ref={pinCodeRef}
-                            onChange={(e) => e.preventDefault()}
-                            onKeyDown={handleInputPin}
-                            onFocus={() => setIsInputFocused(true)}
-                            onBlur={() => setIsInputFocused(false)}
-                          />
-                        </div>
-                        <div
-                          className="pin-page__characters-container"
-                          aria-live="polite"
-                          onClick={() => pinCodeRef.current.focus()}
+                      <div className="visually-hidden">
+                        <label htmlFor="pin-code">{t("enterPINCode")}</label>
+                        <input
+                          type="tel"
+                          name="pin-code"
+                          id="pin-code"
+                          maxLength={4}
+                          pattern="[0-9]*"
+                          autoComplete="off"
+                          value={codePIN}
+                          aria-errormessage="pin-code-error-message"
+                          ref={pinCodeRef}
+                          onChange={(e) => e.preventDefault()}
+                          onKeyDown={handleInputPin}
+                          onFocus={() => setIsInputFocused(true)}
+                          onBlur={() => {
+                            setIsInputFocused(false);
+                            setIsErrorMessageVisible(true);
+                          }}
+                        />
+                      </div>
+                      <div
+                        className="pin-page__characters-container"
+                        aria-live="polite"
+                        onClick={() => pinCodeRef.current.focus()}
+                      >
+                        <span
+                          className={`pin-page__character${
+                            (codePIN.length === 0 || selectedCharIndex === 0) && isInputFocused
+                              ? " focus"
+                              : ""
+                          }`}
+                          aria-hidden="true"
+                          onClick={() => setSelectedCharIndex(0)}
                         >
-                          <span
-                            className={`pin-page__character${
-                              (codePIN.length === 0 || selectedCharIndex === 0) && isInputFocused
-                                ? " focus"
-                                : ""
-                            }`}
-                            aria-hidden="true"
-                            onClick={() => setSelectedCharIndex(0)}
-                          >
-                            {codePIN.charAt(0)}
-                          </span>
-                          <span
-                            className={`pin-page__character${
-                              (codePIN.length === 1 || selectedCharIndex === 1) && isInputFocused
-                                ? " focus"
-                                : ""
-                            }`}
-                            aria-hidden="true"
-                            onClick={() => setSelectedCharIndex(1)}
-                          >
-                            {codePIN.charAt(1)}
-                          </span>
-                          <span
-                            className={`pin-page__character${
-                              (codePIN.length === 2 || selectedCharIndex === 2) && isInputFocused
-                                ? " focus"
-                                : ""
-                            }`}
-                            aria-hidden="true"
-                            onClick={() => setSelectedCharIndex(2)}
-                          >
-                            {codePIN.charAt(2)}
-                          </span>
-                          <span
-                            className={`pin-page__character${
-                              (codePIN.length === 3 || selectedCharIndex === 3) && isInputFocused
-                                ? " focus"
-                                : ""
-                            }`}
-                            aria-hidden="true"
-                            onClick={() => setSelectedCharIndex(3)}
-                          >
-                            {codePIN.charAt(3)}
-                          </span>
-                        </div>
+                          {codePIN.charAt(0)}
+                        </span>
+                        <span
+                          className={`pin-page__character${
+                            (codePIN.length === 1 || selectedCharIndex === 1) && isInputFocused
+                              ? " focus"
+                              : ""
+                          }`}
+                          aria-hidden="true"
+                          onClick={() => setSelectedCharIndex(1)}
+                        >
+                          {codePIN.charAt(1)}
+                        </span>
+                        <span
+                          className={`pin-page__character${
+                            (codePIN.length === 2 || selectedCharIndex === 2) && isInputFocused
+                              ? " focus"
+                              : ""
+                          }`}
+                          aria-hidden="true"
+                          onClick={() => setSelectedCharIndex(2)}
+                        >
+                          {codePIN.charAt(2)}
+                        </span>
+                        <span
+                          className={`pin-page__character${
+                            (codePIN.length === 3 || selectedCharIndex === 3) && isInputFocused
+                              ? " focus"
+                              : ""
+                          }`}
+                          aria-hidden="true"
+                          onClick={() => setSelectedCharIndex(3)}
+                        >
+                          {codePIN.charAt(3)}
+                        </span>
+                      </div>
+                      <p
+                        id="pin-code-error-message"
+                        className={`pin-page__error-message${
+                          isErrorMessageVisible && codePIN.length < 4 ? " visible" : ""
+                        }`}
+                        aria-live="assertive"
+                      >
+                        {t("PINCodeErrorMessage")}
+                      </p>
 
-                        {currentEditingProfile.username === users[0].username && (
-                          <div>
-                            <input
-                              type="checkbox"
-                              name="pin-checkbox-create-new-account"
-                              id="pin-checkbox-create-new-account"
-                              className="checkbox-light"
-                              // ref={pinRequiredInputRef}
-                              // checked={isLockedUser}
-                              // onChange={handleInputChange}
-                            />
-                            <label
-                              htmlFor="pin-checkbox-create-new-account"
-                              className="checkbox-light-label"
-                            >
-                              Wymagaj kodu PIN użytkownika {currentEditingProfile.username}, aby
-                              dodać nowe profile.
-                            </label>
-                          </div>
-                        )}
-                      </fieldset>
+                      {currentEditingProfile.username === users[0].username && (
+                        <div className="pin-page__require-pin-code-wrapper">
+                          <input
+                            type="checkbox"
+                            name="pin-checkbox-create-new-account"
+                            id="pin-checkbox-create-new-account"
+                            className="checkbox-light"
+                            checked={requirePinForNewAccount}
+                            onChange={() => setRequirePinForNewAccount(!requirePinForNewAccount)}
+                          />
+                          <label
+                            htmlFor="pin-checkbox-create-new-account"
+                            className="checkbox-light-label pin-page__label"
+                          >
+                            {t("require")} {currentEditingProfile.username}
+                            {i18n.language === "en" ? `'s` : ","} {t("PINToAddNewProfiles")}.
+                          </label>
+                        </div>
+                      )}
                     </>
                   )}
                 </form>

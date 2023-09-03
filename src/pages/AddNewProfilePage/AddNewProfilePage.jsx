413732768bd20788762b12350b17caa2f7ccb04c
Marcin Jaczewski <78303570+Tryt4n@users.noreply.github.com>
2023-09-03 15:33:58 +0200

    visually done AddNewProfilePage. Added tooltip.

--- src/pages/AddNewProfilePage/AddNewProfilePage.jsx
+++ src/pages/AddNewProfilePage/AddNewProfilePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext, useRef, useEffect } from "react";
 import UserContext from "../../context/UserContext";
+import useWindowSize from "../../hooks/useWindowSize";
 
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
@@ -7,6 +8,7 @@ import { useTranslation } from "react-i18next";
 import Divider from "../../components/Divider/Divider";
 
 import "./addNewProfilePage.scss";
+import "../UserSelectPage/userSelectPage.scss";
 
 export default function AddNewProfilePage() {
   const { t, i18n } = useTranslation();
@@ -21,6 +23,8 @@ export default function AddNewProfilePage() {
   const nameInputRef = useRef();
 
   const navigate = useNavigate();
+
+  const { width } = useWindowSize();
 
   const languageName = Object.keys(languageCodes).find(
     (key) => languageCodes[key] === i18n.language
@@ -46,8 +50,15 @@ export default function AddNewProfilePage() {
       setIsNameValid(true);
     }
   }
+
   function handleCheckboxChange(e) {
     setKidCheckStatus(e.target.checked);
+  }
+
+  function handleCheckboxChangeOnEnter(e) {
+    if (e.key === "Enter") {
+      setKidCheckStatus(!kidCheckStatus);
+    }
   }
 
   function createNewAccount() {
@@ -56,6 +67,7 @@ export default function AddNewProfilePage() {
         id: crypto.randomUUID(),
         username: nameInputValue,
         profileImage: profileImage,
+        kidsProfile: kidCheckStatus,
         language: languageName,
         moviesLanguages: [languageName],
         lock: false,
@@ -88,60 +100,103 @@ export default function AddNewProfilePage() {
 
   return (
     <>
-      <header>
-        <h1>{t("addProfile")}</h1>
-        <p>{t("addProfileDescription")}</p>
-      </header>
+      <main className="choose-profile add-new-profile">
+        <h1 className="choose-profile__header">{t("addProfile")}</h1>
+        <p className="add-new-profile__subheader">{t("addProfileDescription")}</p>
 
-      <Divider />
-
-      <main>
-        <img
-          src={profileImage}
-          alt={t("newAccountDescription")}
+        <Divider
+          spaceSmall={width > 1024 ? false : true}
+          customColor={"#333"}
         />
-        <form onSubmit={(e) => e.preventDefault()}>
-          <label
-            htmlFor="name"
-            className="visually-hidden"
-          >
-            {t("enterName")}
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            value={nameInputValue}
-            ref={nameInputRef}
-            placeholder={t("name")}
-            autoFocus
-            autoComplete="off"
-            aria-invalid={!isNameValid}
-            aria-errormessage="name-error-message"
-            onChange={handleNameInputChange}
-          />
 
-          <input
-            type="checkbox"
-            name="isKid"
-            id="isKid"
-            checked={kidCheckStatus}
-            onChange={handleCheckboxChange}
-          />
-          <label htmlFor="isKid">{t("kid")}?</label>
-          <span
-            id="name-error-message"
-            className={`${isNameValid ? "valid" : "invalid"}`}
-            aria-live="assertive"
-          >
-            {nameInputValue === "" ? t("pleaseEnterName") : t("nameToLong")}
-          </span>
+        <form
+          className="add-new-profile__form"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div className="add-new-profile__img-wrapper">
+            <img
+              src={profileImage}
+              alt={t("newAccountDescription")}
+              className="add-new-profile__img"
+            />
+          </div>
 
-          <Divider />
+          <div className="add-new-profile__wrapper">
+            <div className="add-new-profile__inner-wrapper">
+              <div>
+                <label
+                  htmlFor="name"
+                  className="visually-hidden"
+                >
+                  {t("enterName")}
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="add-new-profile__text-input"
+                  value={nameInputValue}
+                  ref={nameInputRef}
+                  placeholder={t("name")}
+                  autoFocus
+                  autoComplete="off"
+                  aria-invalid={!isNameValid}
+                  aria-errormessage="name-error-message"
+                  onChange={handleNameInputChange}
+                />
+              </div>
+              <div className="add-new-profile__checkbox-tooltip-wrapper">
+                <input
+                  type="checkbox"
+                  name="isKid"
+                  id="isKid"
+                  className="add-new-profile__checkbox-input"
+                  checked={kidCheckStatus}
+                  aria-describedby="checkbox-tooltip"
+                  aria-haspopup="true"
+                  aria-controls="checkbox-tooltip"
+                  onChange={handleCheckboxChange}
+                  onKeyDown={handleCheckboxChangeOnEnter}
+                />
+                <label htmlFor="isKid">{t("kid")}?</label>
+                <span
+                  id="checkbox-tooltip"
+                  className="add-new-profile__checkbox-tooltip"
+                >
+                  {t("kidsTooltip")}
+                </span>
+              </div>
+            </div>
 
-          <button onClick={createNewAccount}>{t("continue")}</button>
-          <button onClick={abortCreatingNewAccount}>{t("cancel")}</button>
+            <span
+              id="name-error-message"
+              className={`add-new-profile__warning-info${isNameValid ? "" : " invalid"}`}
+              aria-live="assertive"
+            >
+              {nameInputValue === "" ? t("pleaseEnterName") : t("nameToLong")}
+            </span>
+          </div>
         </form>
+
+        <Divider
+          spaceSmall={width > 1024 ? false : true}
+          customColor={"#333"}
+        />
+
+        <div className="add-new-profile__btns-wrapper">
+          <button
+            className="add-new-profile__btn add-new-profile__btn--accent"
+            onClick={createNewAccount}
+          >
+            {t("continue")}
+          </button>
+          <button
+            className="add-new-profile__btn add-new-profile__btn--basic"
+            onClick={abortCreatingNewAccount}
+          >
+            {t("cancel")}
+          </button>
+        </div>
       </main>
     </>
   );
